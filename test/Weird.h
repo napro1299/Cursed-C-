94c09d03b0b8ad1fa5f7e8cb38ac6fb0da21a60f
napro1299 <blaketadner@gmail.com>
2020-09-03 00:16:12 -0400

    first commit

---
+++ test/Weird.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#define _USING_WEIRD 1
+
+#if _USING_WEIRD
+
+#define __INT int
+#define __MAIN_CALL main
+#define LEFT_PARAN (
+#define RIGHT_PARAN )
+#define EMPTY_PARAN ()
+#define LEFT_CURLY {
+#define RIGHT_CURLY }
+#define SEMI_COLON ;
+#define __RETURN return
+#define __VOID void
+#define __CONST const
+#define __CHAR char
+#define COLON :
+#define __DBL_COLON ::
+#define LEFT_AGL <
+#define RIGHT_AGL >
+#define __DBL_LEFT_AGL <<
+#define __DBL_RIGHT_AGL >>
+#define DOT .
+
+#define __STANDARD_LIB std
+#define __COUT cout
+#define __ENDL endl
+#define __CIN cin
+#define __GET_FUNC get()
+
+#endif  
+
